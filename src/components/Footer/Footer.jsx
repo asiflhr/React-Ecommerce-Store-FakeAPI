fa126defabf8996a6b59d352815cdec222e0447a
Muhammad Asif <asif88383@gmail.com>
2022-05-14 20:43:04 +0500

    final commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,17 +5,11 @@ import "./footer.css";
 const Footer = () => {
   return (
     <>
-      <Navbar bg="dark" expand="lg" fixed="bottom" className="footer">
-        <Container>
-          <Nav>
-            <div className="text-center mt-2">
-              <h6 className="text-capitalize mb-2 text-warning">
-                T. Kart Inc. 2022 &copy;
-              </h6>
-            </div>
-          </Nav>
-        </Container>
-      </Navbar>
+      <div className="footer">
+        <h6 className="text-capitalize text-warning">
+          T. Kart Inc. 2022 &copy;
+        </h6>
+      </div>
     </>
   );
 };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "././products.css";
 import "../../App.css";
 import { NavLink } from "react-router-dom";
 
